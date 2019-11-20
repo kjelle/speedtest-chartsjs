f4e0c17a7661a6e82a5f3c2aefcab6b8aa06632e
Kjell Tore Fossbakk <kjelltore@gmail.com>
2019-11-20 09:25:25 +0100

    added latency

--- chart.js
+++ chart.js
@@ -1,5 +1,11 @@
+var timeFormat = "MM/DD/YYYY HH:mm";
+
 function bits2mbits(value){
     return (value/1000/1000).toFixed(0).replace(/\d(?=(\d{3})+\.)/g, '$&,') + " mbit/s";
+}
+
+function float2ms(value){
+    return (value).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,') + " ms";
 }
 
 $(document).ready(function() {
@@ -12,14 +18,22 @@ $(document).ready(function() {
     console.log(results);
 
     // Split timestamp and data into separate arrays
-    var labels = [], upload=[], download=[];
+    
+    
+    var labels = [], upload=[], download=[], ping=[];
     results.forEach(function(d) {
-      labels.push(d.timestamp);
+      labels.push(new Date(d.timestamp));
       upload.push(d.upload);
       download.push(d.download);
+      ping.push(d.ping);
     }); 
 
-    $("#details").html('<h3>speedtest-cli@' + results[0].client.ip + ' (' + results[0].client.isp + '@' + results[0].client.country + ')</h3>');
+    $("#details").html('<h3>speedtest-cli from ' + results[0].client.ip + ' hosted by ' + results[0].client.isp + ' (' + results[0].client.country + ')</h3>');
+
+    label_upload = "(" + results[0].server.sponsor + ").upload"; 
+    label_download = "download.{" + results[0].server.sponsor + "}";
+    label_ping = "ping:" + results[0].server.sponsor +"";
+
 
 	var ctx = document.getElementById("myChart").getContext('2d');
 	var myChart = new Chart(ctx, {
@@ -28,28 +42,38 @@ $(document).ready(function() {
 	    labels: labels,
 	    datasets: [
 	      { 
-                label: "Upload",
+                label: label_upload,
                 borderColor: window.chartColors.green,
                 backgroundColor: window.chartColors.green,
                 fill: false,
 		data: upload,
-                yAxisID: "y-axis-upload"
+                yAxisID: "y-axis-mbit"
 	      },
 	      { 
-                label: "Download",
+                label: label_download,
                 borderColor: window.chartColors.blue,
                 backgroundColor: window.chartColors.blue,
                 fill: false,
                 data: download,
-                yAxisID: "y-axis-download"
+                yAxisID: "y-axis-mbit"
+	      },
+	      { 
+                label: label_ping,
+                borderColor: window.chartColors.yellow,
+                backgroundColor: window.chartColors.yellow,
+                fill: false,
+                borderDash: [2, 2],
+                data: ping,
+                yAxisID: "y-axis-ms"
 	      }
 
 	    ]
 	  },
           options: {
+            responsive: true,
 	    scales: {
 	      yAxes: [{
-		id: 'y-axis-download',
+		id: 'y-axis-mbit',
 		type: 'linear',
 		position: 'left',
 	        ticks: {
@@ -57,20 +81,66 @@ $(document).ready(function() {
       		  callback: function(value, index, values) {
 		   return bits2mbits(value);
 		  }
-	        } 
+	        },
+                scaleLabel: {
+                  display: true,
+                  labelString: 'bandwidth'
+                }
 	      }, {
-		id: 'y-axis-upload',
+		id: 'y-axis-ms',
 		type: 'linear',
 		position: 'right',
 	        ticks: {
 		  beginAtZero: true,
       		  callback: function(value, index, values) {
-		   return bits2mbits(value);
+		   return float2ms(value);
 		  }
-	        } 
-              }]
-	    }
+	        },
+                scaleLabel: {
+                  display: true,
+                  labelString: 'latency'
+                }
+
+              }],
+      xAxes: [
+        {
+          type: "time",
+          time: {
+            format: timeFormat,
+//            round: 'day',
+            displayFormats: {
+              'day': 'MMM DD',
+            },
+            tooltipFormat: "ll HH:mm"
+          },
+          scaleLabel: {
+            display: true,
+            labelString: "Date"
+          },
+          ticks: {
+            maxRotation: 0
           }
+        }
+      ],
+	    }
+          },
+
+    pan: {
+      enabled: true,
+      mode: "x",
+      speed: 10,
+      threshold: 10
+    },
+    zoom: {
+      enabled: true,
+      drag: false,
+      mode: "xy",
+      limits: {
+        max: 10,
+        min: 0.5
+      }
+    }
+
 	}); 
     });
 });
